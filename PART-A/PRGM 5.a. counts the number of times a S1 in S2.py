5af8f53798235bfa06ef3e9c985af52303232854
Apek_xa <115926295+apekxa@users.noreply.github.com>
2024-02-12 00:53:32 +0530

    Add files via upload

---
+++ PART-A/PRGM 5.a. counts the number of times a S1 in S2.py
@@ -0,0 +1,10 @@
+str1 = "Welcome to VCET Puttur. VCET awesome, isn't it?"
+sub_string = "VCET"
+
+# convert string to lowercase
+temp_str = str1.lower()
+
+# use count function
+count = temp_str.count(sub_string.lower())
+print("The VCET count is:", count)
+ 
